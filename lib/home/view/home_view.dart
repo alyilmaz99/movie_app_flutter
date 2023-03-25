73a2be48ed613e9859ef4648b2c2f4a7b28f4774
Emre Can Erol <erolemrecan@outlook.com>
2023-03-25 04:10:53 +0300

    feture: bottom navigation bar added

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -11,6 +11,22 @@ class HomePageView extends StatefulWidget {
 class _HomePageViewState extends HomeViewModel {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(36, 42, 50, 1),
+        items: const [
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.search_outlined), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.favorite_outline), label: 'Watch List')
+        ],
+        currentIndex: selectedIndex,
+        onTap: changeSelectedIndex,
+        selectedItemColor: Color.fromRGBO(2, 150, 229, 1),
+        unselectedItemColor: Color.fromRGBO(103, 104, 109, 1),
+      ),
+    );
   }
 }
--- lib/home/viewmodel/home_viewmodel.dart
+++ lib/home/viewmodel/home_viewmodel.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../view/home_view.dart';
 
-abstract class HomeViewModel extends State<HomePageView> {}
+abstract class HomeViewModel extends State<HomePageView> {
+  int selectedIndex = 0;
+
+  void changeSelectedIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+}
