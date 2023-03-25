288a28756f4ed3b25996b8cec45e341388159e7f
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-03-25 04:39:16 +0300

    update: hot fix/colorFix

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -12,20 +12,33 @@ class _HomePageViewState extends HomeViewModel {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromRGBO(36, 42, 50, 1),
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search_outlined), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline), label: 'Watch List')
-        ],
-        currentIndex: selectedIndex,
-        onTap: changeSelectedIndex,
-        selectedItemColor: const Color.fromRGBO(2, 150, 229, 1),
-        unselectedItemColor: const Color.fromRGBO(103, 104, 109, 1),
+      backgroundColor: const Color.fromRGBO(36, 42, 50, 1),
+      body: Column(),
+      bottomNavigationBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.blue, width: 0.4),
+            ),
+          ),
+          child: BottomNavigationBar(
+            elevation: 3,
+            backgroundColor: const Color.fromRGBO(36, 42, 50, 1),
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.search_outlined), label: 'Search'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite_outline), label: 'Watch List'),
+            ],
+            currentIndex: selectedIndex,
+            onTap: changeSelectedIndex,
+            selectedItemColor: const Color.fromRGBO(2, 150, 229, 1),
+            unselectedItemColor: const Color.fromRGBO(103, 104, 109, 1),
+          ),
+        ),
       ),
     );
   }
